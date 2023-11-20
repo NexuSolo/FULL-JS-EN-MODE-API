acb0efd4cceb9be7cd5ee513daa5612dae119b66
Sebastien Zhou <Sebastien.4084@gmail.com>
2023-11-20 15:37:33 +0100

    fix

--- back/src/repository/VoitureRepository.ts
+++ back/src/repository/VoitureRepository.ts
@@ -1,10 +1,10 @@
 import { Pool, QueryResult } from 'pg';
 let pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'covoiturage',
-    password: 'root',
-    port: 4545
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
 });
 
 export class VoitureRepository {
