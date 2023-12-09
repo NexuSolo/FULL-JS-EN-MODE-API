1637568975862908b9490b66b0779e66aa055cf0
Yanis Rozier <yanis.rozier@orange.fr>
2023-12-09 16:54:00 +0100

    Page creation de trajet

--- front/src/components/CreateTrajet.vue
+++ front/src/components/CreateTrajet.vue
@@ -19,6 +19,10 @@
                         <label for="date">Date</label>
                         <input type="date" name="date" id="date">
                     </div>
+                    <div class="create-trajet-form-heure">
+                        <label for="heure">Heure</label>
+                        <input type="time" name="heure" id="heure">
+                    </div>
                     <div class="create-trajet-form-prix">
                         <label for="prix">Prix</label>
                         <input type="text" name="prix" id="prix">
@@ -39,24 +43,97 @@
 </template>
 
 <script>
-
 export default {
     name: 'CreateTrajet',
     props: {
         msg: String
     },
 }
-
 </script>
-
 
 <style scoped>
 
-.create-trajet-fenetre input{
-    height: 30px;
+.create-trajet-form-submit input{
+    width: 100%;
+    background-color: rgb(69, 69, 207);
+    border: none;
     border-radius: 5px;
-    border: rgb(216, 216, 216) solid 1px;
-    margin-bottom: 10px;
-    background-color: rgb(216, 216, 216);
+    padding: 10px;
+    color: white;
+    font-size: 20px;
+    font-weight: 300;
+    cursor: pointer;
 }
+
+.create-trajet-form-submit input:hover{
+    background-color: rgb(121, 121, 233);
+}
+
+.create-trajet-form-submit{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+}
+
+label{
+    font-size: 20px;
+    font-weight: 300;
+}
+
+h1{
+   margin-bottom: 20px;
+}
+
+form input {
+  -webkit-appearance: none;
+     -moz-appearance: none;
+          appearance: none;
+  outline: 0;
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  background-color: rgba(255, 255, 255, 0.2);
+  width: 250px;
+  border-radius: 3px;
+  padding: 10px 15px;
+  display: block;
+  text-align: center;
+  font-size: 18px;
+  color: white;
+  transition-duration: 0.25s;
+  font-weight: 300;
+}
+form input:hover {
+  background-color: rgba(255, 255, 255, 0.4);
+}
+form input:focus {
+  background-color: white;
+  width: 300px;
+  color: rgb(121, 121, 233);
+}
+
+.create-trajet-form{
+    background-color: rgb(206, 206, 206);
+    padding: 20px;
+    border-radius: 10px;
+
+}
+
+.create-trajet-fenetre {
+    width: 1200px;
+    margin: auto;
+}
+
+.create-trajet{
+    width: 600px;
+    margin: auto;
+    padding: 40px;
+}
+
+.create-trajet-form-depart, .create-trajet-form-arrivee, .create-trajet-form-date, .create-trajet-form-heure, .create-trajet-form-prix, .create-trajet-form-places{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 0;
+}
+
 </style>
